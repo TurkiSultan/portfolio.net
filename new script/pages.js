54470e4d7849f3f5847113b53d33aaff95ff322e
TurkiSultan <109402335+TurkiSultan@users.noreply.github.com>
2022-08-20 01:42:27 +0200

    Add files via upload

---
+++ new script/pages.js
@@ -0,0 +1,10 @@
+var mo = document.body;
+function page() {
+    mo.classList.toggle("body")
+}
+function teshert() {
+    mo.classList.toggle("body")
+}
+function moun() {
+    mo.classList.toggle("body")
+}
---
+++ script.js
@@ -0,0 +1,34 @@
+
+ var mo = document.body;
+ var moo = document.getElementById("call");
+ var img = document.getElementsByClassName("logo");
+ var p = document.getElementById("part2");
+ var p2 = document.getElementById("part3");
+ var m1 = document.getElementById("call-2");
+ var n = document.getElementById("part");
+ var m2 = document.getElementById("card-body");
+ var m3 = document.getElementById("card-body2");
+ var m4 = document.getElementById("card-body3");
+ var m5 = document.getElementById("btn-floating1");
+ var m6 = document.getElementById("btn-floating2");
+ var m7 = document.getElementById("btn-floating3");
+ var m8 = document.getElementById("btn-floating4");
+function func() {
+  p.classList.toggle("parting")
+  p2.classList.toggle("parting")
+ mo.classList.toggle("body")
+ moo.classList.toggle("mohamed")
+ m1.classList.toggle("mohamed")
+  n.classList.toggle("mohamed") 
+ m2.classList.toggle("mohamed")
+ m3.classList.toggle("mohamed")
+ m4.classList.toggle("mohamed")
+ m5.classList.toggle("mohamed")
+ m6.classList.toggle("mohamed")
+ m7.classList.toggle("mohamed")
+ m8.classList.toggle("mohamed")
+/*   */
+
+} 
+
+console.log("%c ماذا تريد ياصديقي لا تحاول ان تعبث بالموقع","font-size: x-large;"+"color:red") 
